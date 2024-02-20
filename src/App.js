42f144990b6e575ad970c09ed12d94c8b8bcb4d1
Marcelo Saravia <molinito48@gmail.com>
2024-02-19 23:20:09 -0300

    Estilos añadidos

--- src/App.js
+++ src/App.js
@@ -6,29 +6,24 @@ const App = () => {
   const [dollar, setDollar] = useState("");
   const [arg, setArg] = useState("");
 
-
   const convert = () => {
-    fetch('https://v6.exchangerate-api.com/v6/8f4e3211f3e3b1a49b8792c5/latest/BRL')
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      "https://v6.exchangerate-api.com/v6/8f4e3211f3e3b1a49b8792c5/latest/BRL"
+    )
+      .then((response) => response.json())
+      .then((data) => {
         const dollarValue = real * data.conversion_rates.USD;
         setDollar(dollarValue.toFixed(2));
-        fetch('https://v6.exchangerate-api.com/v6/8f4e3211f3e3b1a49b8792c5/latest/USD')
-          .then(response => response.json())
-          .then(data => {
+        fetch(
+          "https://v6.exchangerate-api.com/v6/8f4e3211f3e3b1a49b8792c5/latest/USD"
+        )
+          .then((response) => response.json())
+          .then((data) => {
             const argValue = dollarValue * data.conversion_rates.ARS * 1.31;
             setArg(argValue.toFixed(2));
           });
       });
   };
-   
-
- /*  const convert = () => {
-    const dollarValue = real * 0.19;
-    setDollar(dollarValue.toFixed(2));
-    const argValue = dollarValue * 835 * 1.31;
-    setArg(argValue.toFixed(2));
-  }; */
 
   return (
     <div className="container">
@@ -39,17 +34,20 @@ const App = () => {
         value={real}
         onChange={(e) => setReal(e.target.value)}
       />
-      <br className="button" />
-      <button onClick={convert}>Convert</button>
-      <button onClick={() => setReal("")}>Reset</button>
+      <br />
+      <div className="button">
+        <button onClick={convert}>Convert</button>
+        <button onClick={() => setReal("")}>Reset</button>
+      </div>
       <p>Dollar: {dollar}</p>
       <p>Pes.Arg: {arg}</p>
 
       <div className="dolarhoy">
-        <iframe title="Dolar hoy" src="https://dolarhoy.com/i/cotizaciones/dolar-blue"></iframe>
-
+        <iframe
+          title="Dolar hoy"
+          src="https://dolarhoy.com/i/cotizaciones/dolar-blue"
+        ></iframe>
       </div>
-      
 
       <div className="footer">
         <p>
@@ -57,10 +55,13 @@ const App = () => {
           <a href="zingy-puppy-bdd4d4.netlify.app/">Marcelo Saravia</a>
         </p>
       </div>
+      <div>
+        <h3>
+          Powered by: <a href="https://dolarhoy.com/">DolarHoy</a>
+        </h3>
+      </div>
     </div>
   );
 };
 
-export default App; 
-
-
+export default App;
